275dc27a24ce1711b482a64a9f65bf7a515f190c
unknown <irispbmink@gmail.com>
2025-03-31 12:43:22 +0300

    init

--- app/src/main/java/com/example/osnovaprj/ui/screen/signIn/SignInScreen.kt
+++ app/src/main/java/com/example/osnovaprj/ui/screen/signIn/SignInScreen.kt
@@ -1,14 +1,19 @@
 package com.example.osnovaprj.ui.screen.signIn
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.VisualTransformation
@@ -41,10 +47,21 @@ fun SignInContent(){
             title = stringResource(R.string.hello),
             subTitle = stringResource(R.string.sign_in_subtitle)
         )
+        val email = remember { mutableStateOf("") }
+        Spacer(modifier = Modifier.height(35.dp))
+        AuthTextFiled(
+            labelText = "Email",
+            placeHolderText = stringResource(R.string.template_email),
+            value = email.value,
+            onChangeValue = {
+                email.value = it
+            }
+        )
+
     }
 }
 
-@Preview
+
 @Composable
 fun TitleWithSubtitleText(title: String, subTitle: String){
     Column(
@@ -63,30 +80,66 @@ fun TitleWithSubtitleText(title: String, subTitle: String){
             style = MatuleTheme.typography.subTitleRegular16.copy(color = MatuleTheme.colors.subTextDark),
             textAlign = TextAlign.Center
         )
+
+
     }
 
 }
 
-@Preview
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AuthTextFiled(){
-    val value = remember { mutableStateOf("") }
-    val  interaction = remember { MutableInteractionSource() }
-    BasicTextField(
-        value = value.value,
-        onValueChange = {
-            value.value = it
-        }
-    ){innerTextField ->
-        TextFieldDefaults.DecorationBox(
-            value = value.value,
-            singleLine = true,
-            innerTextField = innerTextField,
-            enabled = true,
-            visualTransformation = VisualTransformation.None,
-            interactionSource = interaction
-        )
+fun AuthTextFiled(value: String, onChangeValue: (String) -> Unit, placeHolderText: String? = null, labelText: String? = null){
 
+    Column(
+       modifier = Modifier
+           .padding(horizontal = 20.dp)
+           .wrapContentSize(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        if (labelText != null){
+            Text(
+                text = labelText,
+                style = MatuleTheme.typography.bodyRegular16.copy(MatuleTheme.colors.text),
+                textAlign = TextAlign.Right
+            )
+        }
+        val  interaction = remember { MutableInteractionSource() }
+        BasicTextField(
+            value = value,
+            onValueChange = { onChangeValue(it)},
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .background(MatuleTheme.colors.background)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(14.dp))
+        ){innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                value = value,
+                singleLine = true,
+                innerTextField = innerTextField,
+                enabled = true,
+                visualTransformation = VisualTransformation.None,
+                interactionSource = interaction,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MatuleTheme.colors.background,
+                    disabledContainerColor = MatuleTheme.colors.background,
+                    unfocusedContainerColor = MatuleTheme.colors.background,
+                    errorContainerColor = MatuleTheme.colors.background,
+                    focusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
+                ),
+                placeholder = {
+                    if (placeHolderText != null)
+                    Text(
+                        text = placeHolderText,
+                        style = MatuleTheme.typography.bodyRegular14.copy(color = MatuleTheme.colors.hint)
+                    )
+                }
+            )
+
+        }
     }
+
 }
