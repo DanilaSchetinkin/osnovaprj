8d64b649656b279ce9fef608e885bebb717a7acf
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-04-28 12:52:41 +0300

    init reg

--- app/src/main/java/com/example/osnovaprj/MainActivity.kt
+++ app/src/main/java/com/example/osnovaprj/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.osnovaprj.ui.screen.registration.screen.Registration
 import com.example.osnovaprj.ui.screen.signIn.screen.SignInScreen
 import com.example.osnovaprj.ui.theme.MatuleTheme
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MatuleTheme {
-                SignInScreen()
+                Registration()
             }
         }
     }
--- app/src/main/java/com/example/osnovaprj/ui/screen/registration/screen/RegistrationState.kt
+++ app/src/main/java/com/example/osnovaprj/ui/screen/registration/screen/RegistrationState.kt
@@ -1,6 +1,6 @@
-package com.example.osnovaprj.ui.screen.Regestration.screen
+package com.example.osnovaprj.ui.screen.registration.screen
 
-data class RegistrationInState (
+data class RegistrationState (
     var name: String ="",
     var email:String ="",
     var password:String ="",
